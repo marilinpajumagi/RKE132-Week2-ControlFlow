1d9b5038cb50065a6acda445950d06b238778c99
Marilin Pajumägi <marilin.pajumagi@tktk.ee>
2023-12-17 15:47:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, world!");
+//uued andmetüübid: int ja char
+//if-statements (control flow)
